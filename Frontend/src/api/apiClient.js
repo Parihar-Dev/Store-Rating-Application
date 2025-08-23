08f91b33891ccd2b0e3f7c0f5bb63c903467f868
Rahul Parihar <pariharr0203@gmail.com>
2025-08-24 04:17:54 +0530

    Adding backend live url

--- Frontend/src/api/apiClient.js
+++ Frontend/src/api/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api'; 
+const API_URL = 'https://store-rating-application.onrender.com'; 
 
 const apiClient = axios.create({
   baseURL: API_URL,
